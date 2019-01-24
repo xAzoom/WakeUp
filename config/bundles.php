2a1fd8c2d4e42656ef443f776916cbe9cdffe7b9
xAzoom <morenequs@gmail.com>
2019-01-24 11:10:10 +0100

    Add apache-pack (.htaccess)

--- config/bundles.php
+++ config/bundles.php
@@ -10,7 +10,7 @@ return [
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['all' => true],
-    Symfony\Bundle\WebServerBundle\WebServerBundle::class => ['dev' => true, 'test' => true],
+    Symfony\Bundle\WebServerBundle\WebServerBundle::class => ['all' => true],
     Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
     FOS\RestBundle\FOSRestBundle::class => ['all' => true],
